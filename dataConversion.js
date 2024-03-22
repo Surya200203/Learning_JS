f49bc7d095d9ce5a0f46bdaf0f7ae10a29c9a2e4
Sanket Suryavanshi <sanket20020312@gmail.com>
2024-03-22 10:44:31 +0530

    Added dataConversion.js file

---
+++ dataConversion.js
@@ -0,0 +1,18 @@
+//Data Conversion
+
+let score = '123';
+let points = '123abc';
+
+let valueInteger = Number(score);
+let value2 = Number(points);
+
+console.log("Score : "+ valueInteger);
+console.log("points : "+ value2);
+
+/*
+    OUTPUT
+
+    Score : 123
+    points : NaN
+
+*/
