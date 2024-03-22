9a07f129f5426c745546764cd99906f791dbb1f3
Sanket Suryavanshi <sanket20020312@gmail.com>
2024-03-22 10:44:12 +0530

    Added dataType.js file

---
+++ dataType.js
@@ -0,0 +1,26 @@
+//'use strict';   // Treat all JS code as newer version
+
+//Data Types
+
+let firstName = "Sanket";    // String
+let num = 2;            // Integer
+let isLoggedIn = true;  // Boolean (true/false)
+let number = null;      // Null (standalone value) (empty)
+let car ;               // undefined
+
+console.log(firstName);
+console.log(num);
+console.log(isLoggedIn);
+console.log(number);
+console.log(car);
+
+/*
+OUTPUT
+
+Sanket
+2
+true
+null
+undefined
+
+*/
