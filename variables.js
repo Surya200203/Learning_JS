72ca7a405c10816e74ed4f293fa4bdd1c175c49e
Sanket Suryavanshi <sanket20020312@gmail.com>
2024-03-22 10:43:44 +0530

    Added variables.js file

---
+++ variables.js
@@ -0,0 +1,19 @@
+console.log('Sanket');
+
+// There are var, const and let three keywords used for defining variables in javascript
+
+// 1. const
+
+const accountID = 1234;     // this id immutable (can't change once defined)
+
+// 2. var
+
+var accountEmail = "sanket@gmail.com";  // it has functional and block scope (not prefered to use)
+
+// 3. let
+
+let carName = "Creta";  // prefered to use
+
+
+// To print all variables in one table
+console.table([accountID,accountEmail,carName]);
