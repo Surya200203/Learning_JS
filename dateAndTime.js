cde11495899034ae5af0e30c311d72d99e42d9ea
Sanket Suryavanshi <sanket20020312@gmail.com>
2024-03-22 10:45:08 +0530

    Added dateAndTime.js file

---
+++ dateAndTime.js
@@ -0,0 +1,29 @@
+
+//Date
+
+let myDate = new Date(); //Date is an object in JS
+console.log(myDate);
+console.log(myDate.toString());
+
+/*
+
+OUTPUT
+
+2024-03-22T04:17:48.848Z
+Fri Mar 22 2024 09:47:48 GMT+0530 (India Standard Time)
+
+
+
+*/
+
+//Time
+
+let myTime = Date.now();
+console.log(myTime);
+
+/*
+
+OUTPUT
+
+1711081151153 (in milliseconds)
+*/
